8143b3fd5093ff3c2d05c6c83e2ed6436289cb48
Cho seyun <joseyun94@naver.com>
2025-01-02 16:43:15 +0900

    No_25314

---
+++ src/baekjoon/No_25314.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_25314 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        while (N != 0) {
+            sb.append("long ");
+            N -= 4;
+        }
+        sb.append("int");
+
+        System.out.println(sb.toString());
+    }
+}

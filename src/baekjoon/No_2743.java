47c592463502225b22c4e0a8c180ff948bb64ac7
Cho seyun <joseyun94@naver.com>
2025-01-03 15:30:05 +0900

    No_2743

---
+++ src/baekjoon/No_2743.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_2743 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}

b89a65ed4d96432fef5089aad2e4b8e39d236b81
Cho seyun <joseyun94@naver.com>
2025-01-03 16:04:25 +0900

    No_11654

---
+++ src/baekjoon/No_11654.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_11654 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int) br.readLine().toCharArray()[0]);
+    }
+}

6b68da920778f6591ee2b4d10af4e8e1643ee2d5
Cho seyun <joseyun94@naver.com>
2025-02-04 22:17:59 +0900

    No_1058

---
+++ src/baekjoon/No_1058.java
@@ -0,0 +1,67 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_1058 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] dist = new int[N + 1][N + 1];
+        int INF = 100_000_000;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                dist[i][j] = INF;
+            }
+        }
+
+        StringTokenizer st;
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                int a = Integer.parseInt(st.nextToken());
+                if (a == 1) {
+                    dist[i][j] = 1;
+                }
+            }
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    sb.append(1 + " ");
+                    continue;
+                }
+
+                if (dist[i][j] == INF) {
+                    sb.append(0 + " ");
+                } else {
+                    sb.append(1 + " ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}

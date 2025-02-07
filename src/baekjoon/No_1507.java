5620641b8fa61332a9c674c57f3681ace3bd582b
Cho seyun <joseyun94@naver.com>
2025-02-07 19:48:19 +0900

    No_1507

--- src/baekjoon/No_1507.java
+++ src/baekjoon/No_1507.java
@@ -13,13 +13,48 @@ public class No_1507 {
         int N = Integer.parseInt(br.readLine());
 
         int[][] dist = new int[N + 1][N + 1];
+        int[][] arr = new int[N + 1][N + 1];
+        boolean[][] visited = new boolean[N + 1][N + 1];
+        int INF = 100_000_000;
 
         StringTokenizer st;
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
                 dist[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = dist[i][j];
             }
         }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (i == j || i == k || j == k) {
+                        continue;
+                    }
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        System.out.println(-1);
+                        return;
+                    }
+
+                    if (dist[i][j] == dist[i][k] + dist[k][j]) {
+                        arr[i][j] = INF;
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (arr[i][j] == INF || i == j || visited[i][j]) {
+                    continue;
+                }
+                answer += arr[i][j];
+                visited[i][j] = true;
+                visited[j][i] = true;
+            }
+        }
+        System.out.println(answer);
     }
 }

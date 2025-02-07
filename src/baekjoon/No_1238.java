935bfebc97cbe16e503facc0b08cf0db9b0e609b
Cho seyun <joseyun94@naver.com>
2025-02-07 19:49:00 +0900

    No_1238

---
+++ src/baekjoon/No_1238.java
@@ -0,0 +1,59 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_1238 {
+
+    static int N, M, X;
+    static int[][] dist;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        dist = new int[N + 1][N + 1];
+        int inf = 10_000_001;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                dist[i][j] = inf;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = c;
+        }
+
+        floyd();
+        int answer = 0;
+
+        for (int i = 1; i <= N; i++) {
+            answer = Math.max(answer, dist[i][X] + dist[X][i]);
+        }
+        System.out.println(answer);
+    }
+
+    private static void floyd() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+}

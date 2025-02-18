c059b1263a298b1a4f113e7bbd38ec0990b56b12
Cho seyun <joseyun94@naver.com>
2025-02-18 23:33:49 +0900

    No_10824

---
+++ src/baekjoon/No_10824.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_10824 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String a = st.nextToken() + st.nextToken();
+        String b = st.nextToken() + st.nextToken();
+
+        long answer = Long.parseLong(a) + Long.parseLong(b);
+        System.out.println(answer);
+    }
+}

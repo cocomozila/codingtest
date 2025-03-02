ada04b476490d19d66770d63a0e4706e2d8c631e
Cho seyun <joseyun94@naver.com>
2025-03-03 03:57:09 +0900

    No_15000

---
+++ src/baekjoon/No_15000.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_15000 {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(new BufferedReader(new InputStreamReader(System.in)).readLine().toUpperCase());
+    }
+}

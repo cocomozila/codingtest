0df7c41cd1a636aa789e90f484b6ce0d2075332f
Cho seyun <joseyun94@naver.com>
2025-03-08 01:27:37 +0900

    No_1212

--- src/baekjoon/No_1212.java
+++ src/baekjoon/No_1212.java
@@ -7,9 +7,29 @@ import java.io.InputStreamReader;
 public class No_1212 {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int i = Integer.parseInt(br.readLine(), 8);
-        System.out.println(Integer.toString(i, 2));
+        String octal = br.readLine();
+
+        if(octal.equals("0")) {
+            System.out.println("0");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int firstDigit = octal.charAt(0) - '0';
+        sb.append(Integer.toBinaryString(firstDigit));
+
+        for (int i = 1; i < octal.length(); i++) {
+            int digit = octal.charAt(i) - '0';
+            String bin = Integer.toBinaryString(digit);
+
+            while (bin.length() < 3) {
+                bin = "0" + bin;
+            }
+            sb.append(bin);
+        }
+
+        System.out.println(sb.toString());
     }
 }

0783f49e8a12990344a7407dd6b64e2d4fd11613
Cho seyun <joseyun94@naver.com>
2025-03-14 02:21:48 +0900

    No_1713

--- src/baekjoon/No_1713.java
+++ src/baekjoon/No_1713.java
@@ -3,7 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class No_1713 {
 
@@ -16,9 +16,89 @@ public class No_1713 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int[] arr = new int[M];
+        Student[] students = new Student[101];
+        int[] recommendations = new int[101];
+
+        for (int i = 0; i <= 100; i++) {
+            students[i] = new Student(i, 0);
+        }
 
         for (int i = 0; i < M; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Queue<Student> queue = new PriorityQueue<>(){};
+
+        for (int i = 0; i < M; i++) {
+            int number = arr[i];
+
+            if (queue.size() < N) {
+                if (queue.contains(students[number])) {
+                    queue.remove(students[number]);
+                    recommendations[number] += 1;
+                    queue.add(new Student(number, recommendations[number], students[number].time));
+                } else {
+                    queue.add(new Student(number, 0, i));
+                    students[number].time = i;
+                }
+            } else {
+                if (queue.contains(students[number])) {
+                    queue.remove(students[number]);
+                    recommendations[number] += 1;
+                    queue.add(new Student(number, recommendations[number], students[number].time));
+                } else {
+                    Student poll = queue.poll();
+                    recommendations[poll.number] = 0;
+                    students[poll.number].time = 0;
+                    queue.add(new Student(number, 0, i));
+                    students[number].time = i;
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            list.add(queue.poll().number);
+        }
+
+        Collections.sort(list);
+        StringBuilder sb = new StringBuilder();
+        for (int number : list) {
+            sb.append(number + " ");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static class Student implements Comparable<Student> {
+        int number;
+        int recommend = 0;
+        int time;
+
+        public Student(int number, int time) {
+            this.number = number;
+            this.time = time;
+        }
+
+        public Student(int number, int recommend, int time) {
+            this.number = number;
+            this.recommend = recommend;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            if (this.recommend == o.recommend) {
+                return this.time - o.time;
+            } else {
+                return this.recommend - o.recommend;
+            }
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return this.number == ((Student) obj).number;
         }
     }
 }

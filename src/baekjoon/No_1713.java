907833d1b1b6c588febb3e6ba0c44d52aea2629e
Cho seyun <joseyun94@naver.com>
2025-03-13 23:54:48 +0900

    No_1713

---
+++ src/baekjoon/No_1713.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_1713 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[M];
+
+        for (int i = 0; i < M; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
